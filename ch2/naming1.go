49122666380bc4f14214e519a39eb9db359bf384
ColombianJoker <elbarrios1@poligran.edu.co>
2025-07-25 17:18:06 -0500

    naming1, unused1

---
+++ ch2/naming1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	_0 := 0_0
+	_𝟙 := 20
+	𝑛 := 3
+	ａ := "hello" // Unicode U+FF41
+	fmt.Println(_0)
+	fmt.Println(_𝟙)
+	fmt.Println(𝑛)
+	fmt.Println(ａ)
+
+	ａ = "hello"    // Unicode U+FF41
+	a := "goodbye" // standard lowercase latin a
+	fmt.Println(ａ)
+	fmt.Println(a)
+}
