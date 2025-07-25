d63be16776deb202619cdefae42721dce5ddc6be
ColombianJoker <elbarrios1@poligran.edu.co>
2025-07-25 18:37:02 -0500

    ch3/arrays1.go ch3/slices1.go

---
+++ ch3/arrays1.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x = [12]int{1, 5: 4, 5, 10: 100, 15}
+
+	fmt.Println("Values in x:")
+	for i, v := range x {
+		fmt.Printf("x[%d] = %d\n", i, v)
+	}
+
+	fmt.Println("")
+
+	for i := 0; i < len(x); i++ {
+		fmt.Printf("x[%d] = %d\n", i, x[i])
+	}
+}
