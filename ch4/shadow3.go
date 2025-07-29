f01bb4dfcbc3e2d24dc282fef4a611fb339d8970
ColombianJoker <elbarrios1@poligran.edu.co>
2025-07-29 09:47:53 -0500

    if block

--- ch4/shadow3.go
+++ ch4/shadow3.go
@@ -9,4 +9,7 @@ func main() {
 	fmt.Println("x:", x)
 	fmt := "oops"
 	fmt.Println("x:", x)
+	// go run ch4/shadow3.go
+	// # command-line-arguments
+	// ch4/shadow3.go:11:6: fmt.Println undefined (type string has no field or method Println)
 }
