ec25faaf78210321f8100dcdb523465c60a452db
ColombianJoker <elbarrios1@poligran.edu.co>
2025-08-04 17:10:25 -0500

    Pass by value

---
+++ ch5/fn9.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type person struct {
+	age  int
+	name string
+}
+
+func modifyFails(i int, s string, p person) {
+	i = i * 2
+	s = "Goodbye"
+	p.name = "Bob"
+}
+
+func main() {
+	p := person{}
+	i := 2
+	s := "Hello"
+	fmt.Println(i, s, p)
+	modifyFails(i, s, p)
+	fmt.Println(i, s, p)
+}
